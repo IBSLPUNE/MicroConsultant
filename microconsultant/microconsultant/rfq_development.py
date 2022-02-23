db941fd76c9e070cdeaf0b1fae61554b22ef2706
Shantanu-mishra <shantanunihal3@gmail.com>
2022-02-23 16:19:47 +0530

    "2nd"

--- microconsultant/microconsultant/rfq_development.py
+++ microconsultant/microconsultant/rfq_development.py
@@ -17,39 +17,38 @@ from frappe import _, msgprint
 
 def add_items(self, method):
 	stock_dic={}
-	for d in self.get("mr_items"):
+	items = self.get("mr_items")
+	for d in items[:]:
 		qty_oh=0.0
 		qty_or=0.0
 		stock = frappe.db.get_value("Bin",{'item_code':d.item_code,'warehouse':d.warehouse},"actual_qty")
 		stock_dic.update({d.item_code:stock})
 		altic = frappe.db.sql_list("""SELECT alternative_item_code FROM `tabItem Alternative` WHERE item_code = %s""",d.item_code)
-		if altic == None:
-			break
 		for a in altic:
 			alt_stocks = frappe.db.get_value("Bin",{'item_code':a,'warehouse':d.warehouse},"actual_qty")
 			if a in stock_dic:
+				print("%s stock",a)
 				break
 			stock_dic.update({a:alt_stocks})
 			if stock_dic[a] == None :
 				stock_dic.update({a:0})
 			qty_oh = qty_oh + stock_dic[a]
-		print(stock_dic)
 		qty_oh += d.actual_qty
 		qty_or = d.required_bom_qty - qty_oh
-		print(qty_or)
-		if qty_or >= 0:
-			d.quantity = qty_or
+		if qty_or <= 0:
+			self.remove(d)
+			message = _("As there are sufficient raw materials alternate included, Material Request is not required for Warehouse {0}.").format(d.warehouse) + "<br><br>"
+			frappe.msgprint(message, title=_("Note"))
 		else:
-			d.remove()
-			message = _("As there are sufficient raw materials, Material Request is not required for Warehouse {0}.").format(self.warehouse) + "<br><br>"
-			rappe.msgprint(message, title=_("Note"))
+			d.quantity = qty_or
+			#d.save()
 		for a in altic:
 			if qty_or >= 0:
 				stock_dic.update({a:0})
 			else:
 				req_qty = d.required_bom_qty - d.actual_qty
-				sorted_stock ={k: v for k,v in sorted(stock_dic.item(), key= lambda v: v[1])}
-				for x,y in sorted_stock.item():
+				sorted_stock ={k: v for k,v in sorted(stock_dic.items(), key= lambda v: v[1])}
+				for x,y in sorted_stock.items():
 					req_qty -= y
 					if req_qty < 0:
 						break
@@ -69,6 +68,9 @@ def rfq_items(self, method):
 		for i in range(0, len(altic)):
 			item_supplier = frappe.db.get_value('Item Supplier',{"parent":'Item', "parent":altic[i]},'supplier')
 			item_manufacturers = frappe.db.sql_list("""SELECT manufacturer FROM `tabItem Manufacturer` WHERE item_code = %s""",altic[i])
+			print(item_manufacturers)
+			if item_manufacturers == []:
+				frappe.throw("Add item_manufacturers for item "+ altic[i])
 			for s in self.get('suppliers'):
 				print(s)
 				for m in range(0, len(item_manufacturers)):
