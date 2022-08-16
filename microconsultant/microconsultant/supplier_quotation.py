d939387d81865a92b8cf7c679fb21a1f42299b71
IBSLPUNE <31270467+IBSLPUNE@users.noreply.github.com>
2022-08-16 12:49:57 +0530

    Update supplier_quotation.py

--- microconsultant/microconsultant/supplier_quotation.py
+++ microconsultant/microconsultant/supplier_quotation.py
@@ -3,5 +3,6 @@ import frappe
 def filter_items(self, method):
 	items = self.get('items')
 	for item in items[:]:
+		frappe.throw(item.item_code)
 		if not frappe.db.exists("Item Supplier",{"supplier":self.supplier, "parent":item.item_code}):
 			self.remove(item)
