12116dd7ffd28c8fcd8d15acd1e9a0bcccc2570d
IBSLPUNE <31270467+IBSLPUNE@users.noreply.github.com>
2023-02-28 22:13:21 +0530

    Update rfq_development.py

--- microconsultant/microconsultant/rfq_development.py
+++ microconsultant/microconsultant/rfq_development.py
@@ -106,6 +106,46 @@ def psalt(self):
 											break
 									req_qty = abs(req_qty)
 									stock_dic.update({x:req_qty})
+	for k in self.get("sub_assembly_items"):
+		product_specific = frappe.db.sql_list("""SELECT alternatives FROM `tabAlt Items` WHERE parent=%s""",k.production_item)
+		for p in product_specific:
+			itm = frappe.db.sql_list("""SELECT item_code FROM `tabItem Alternative` WHERE alternative_item_code = %s AND product_specific_alternatives=1""",p)
+			if itm != []:
+				it = self.get("mr_items")
+				for d in it[:]:
+					for z in itm:
+						if d.item_code == z:
+							qty_oh=0.0
+							qty_or=0.0
+							alt_stock=0.0
+							alt_stocks = frappe.db.sql_list("""SELECT projected_qty FROM `tabBin` WHERE item_code=%s""",p)
+							for k in alt_stocks:
+								alt_stock = alt_stock +k
+							if alt_stock>0:
+								if d in stock_dic:
+									break
+								stock_dic.update({p:alt_stock})
+								if stock_dic[p] == None :
+									stock_dic.update({p:0})
+								qty_oh = qty_oh + stock_dic[p]
+								qty_or = d.quantity - qty_oh
+								if qty_or <= 0:
+									self.remove(d)
+									message = _("As there are sufficient raw materials included, Material Request is not required for Warehouse {0}.").format(d.warehouse) + "<br><br>"
+									frappe.msgprint(message, title=_("Note"))
+								else:
+									d.quantity = qty_or
+								if qty_or >= 0:
+									stock_dic.update({p:0})
+								else:
+									req_qty = d.quantity
+									sorted_stock ={k: v for k,v in sorted(stock_dic.items(), key= lambda v: v[1])}
+									for x,y in sorted_stock.items():
+										req_qty -= y
+										if req_qty < 0:
+											break
+									req_qty = abs(req_qty)
+									stock_dic.update({x:req_qty})
 
 						
 
@@ -215,7 +255,35 @@ def rfq_ps(self):
 											doc.set_missing_values()
 											row.insert()
 											break
-
+		for i in p.get("sub_assembly_items"):	
+			product_specific = frappe.db.sql_list("""SELECT alternatives FROM `tabAlt Items` WHERE parent=%s""",i.production_item)
+			for p in product_specific:
+				itm = frappe.db.sql_list("""SELECT item_code FROM `tabItem Alternative` WHERE alternative_item_code = %s AND product_specific_alternatives=1""",p)
+				if itm != []:
+					it = self.get("items")
+					for d in it[:]:
+						for z in itm:
+							if d.item_code == z:
+								item_manufacturers = frappe.db.sql_list("""SELECT manufacturer_part_no FROM `tabItem Manufacturer` WHERE item_code = %s""",p)
+								if item_manufacturers == []:
+									frappe.throw("Add item_manufacturers for item "+ p)
+								for m in range(0, len(item_manufacturers)):
+									for s in self.get('suppliers'):
+										if frappe.db.exists("Item Supplier",{'parent':p, 'supplier':s.supplier}):
+											doc = frappe.get_doc(self)
+											row = doc.append('items',{})
+											row.item_code = p
+											row.qty = d.qty
+											row.alternate = 1
+											row.product_name = d.product_name
+											row.idx = d.idx + 1
+											row.alternate_of = d.item_code
+											row.warehouse = d.warehouse
+											row.schedule_date = d.schedule_date
+											row.manufacturer_part_no = item_manufacturers[m]
+											doc.set_missing_values()
+											row.insert()
+											break
 
 
 
